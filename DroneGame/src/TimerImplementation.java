9f956d589692bc99de0803def49d56480cf99c23
avanibhatnagar <avanibhatnagar07@gmail.com>
2018-12-04 12:43:03 -0800

    timer countdown

--- DroneGame/src/TimerImplementation.java
+++ DroneGame/src/TimerImplementation.java
@@ -9,17 +9,21 @@ public class TimerImplementation extends JLabel {
 
     public boolean bool = false;
 
-    int sec = 0;
-    int min = 0;
+    int sec = 30;
+    int min = 1;
 
     Timer timer = new Timer();
     TimerTask task = new TimerTask() {
         public void run() {
-            if (sec < 59)
-                sec++;
-            else {
-                sec = 0;
-                min++;
+            if (sec > 0)
+                sec--;
+            else if(min==1) {
+                min--;
+                sec = 59;
+            }
+            else{
+                min=0;
+                sec=0;
             }
             if (sec < 10 && min < 10)
                 label.setText("Timer: 0" + Integer.toString(min) + ":0" + Integer.toString(sec));
@@ -30,11 +34,22 @@ public class TimerImplementation extends JLabel {
         }
     };
 
+    public int getSec(){
+        return sec;
+    }
+    public int getMin(){
+        return min;
+    }
+
     public TimerImplementation() {
         bool = true;
         timer.scheduleAtFixedRate(task, 1000, 1000);
         this.setHorizontalAlignment(SwingConstants.RIGHT);
 //		this.setBounds(500, 400, 75, 75);
+    }
+    public void resetTimer(){
+        sec = 30;
+        min = 1;
     }
 }
 
